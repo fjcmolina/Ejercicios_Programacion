67050d0ef5731385dd8d797e00282938fe97460d
Francisco Jesús Caballero Molina <fjcmolina7@gmail.com>
2017-11-01 13:47:00 +0100

    Actualizando ejercicios

--- Bucles/Ejercicio19.java
+++ Bucles/Ejercicio19.java
@@ -11,10 +11,28 @@
     
     System.out.println("Vamos a realizar un programa para que pinte una piramide");
     System.out.println("Introduce la altura de la piramide");
-    int altura = Integer.parseInt(System.console().readLine());
+    int alturaIntroducida = Integer.parseInt(System.console().readLine());
     
     System.out.println("Introduce el caracter que deseas para pintar la piramide");
     String pintar = System.console().readLine();
-  
+    
+    int altura = 1;
+    int espacios = alturaIntroducida - 1;
+    
+    while( altura <= alturaIntroducida){
+      
+      for(int i = 1; i <= espacios; i++){
+        System.out.print(" ");
+        }
+        
+      for(int i = 1; i <= altura * 2; i++){
+        System.out.print(pintar);
+        }
+        
+      System.out.println();
+      
+      altura++;
+      espacios--;
+    }
   }
 }
