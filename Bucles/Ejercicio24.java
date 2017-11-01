0027f41fc7f78e7ee88523ab025b84936ad7f925
Francisco Jesús Caballero Molina <fjcmolina7@gmail.com>
2017-11-01 17:27:21 +0100

    Actualizando ejercicios

--- Bucles/Ejercicio24.java
+++ Bucles/Ejercicio24.java
@@ -7,5 +7,33 @@
  
  public class Ejercicio24{
   public static void main(String[] args) {
+    
+    System.out.println("Vamos a relaizar un programa que pinta una piramide de numeros");
+    
+    System.out.println("Introduce la altura de la piramide");
+    int alturaIntroducida = Integer.parseInt(System.console().readLine());
+    
+    int altura = 1;
+    int espacios = alturaIntroducida - 1;
+    int i = 0;
+    
+    while( altura <= alturaIntroducida){
+      
+      for(i = 1; i <= espacios; i++){
+        System.out.print(" ");
+        }
+        
+      for(i = 1; i <= altura  - 1; i++){
+        System.out.print(i);
+        }
+      for(i = altura; i > 0; i--){
+        System.out.print(i);
+        }
+        
+      System.out.println();
+      
+      altura++;
+      espacios--;
+    }
   }
 }
