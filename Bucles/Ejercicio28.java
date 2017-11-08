54dc903461cdd17bcbfbe84604a247c4e76c7054
Francisco Jesús Caballero Molina <fjcmolina7@gmail.com>
2017-11-08 17:41:32 +0100

    Actualizando ejercicios

--- Bucles/Ejercicio28.java
+++ Bucles/Ejercicio28.java
@@ -7,5 +7,16 @@
  
  public class Ejercicio28{
   public static void main(String[] args) {
+    
+    System.out.println("Vamos a realizar un programa que calcula el factorial del numero introducido");
+    System.out.println("Introduce un numero:");
+    int numero= Integer.parseInt( System.console().readLine());
+    
+    int factorial = 1;
+    
+    for (int i = 1 ; i <= numero ; i ++) {
+        factorial *= i;
+      }
+    System.out.println("El factorial de " + numero + " es " + factorial);
   }
 }
--- Bucles/Ejercicio29.java
+++ Bucles/Ejercicio29.java
@@ -8,5 +8,21 @@
  
  public class Ejercicio29{
   public static void main(String[] args) {
+    
+    System.out.println("Vamos a realizar un programa que muestra los numeros positivos menores divisibles entre otro numero");
+    System.out.println("Introduce el numero positivo mayor: ");
+    int numeroMayor= Integer.parseInt( System.console().readLine());
+    
+    System.out.println("Introduce un numero menor que el anterior: ");
+    int numeroMenor= Integer.parseInt( System.console().readLine());
+    
+    System.out.println("Los siguientes numeros menores que " + numeroMayor+
+    " no son divisibles entre " + numeroMenor);
+    
+    for(int i = 1; i < numeroMayor;i++){
+      if((i % numeroMenor)!= 0){
+        System.out.println(i);
+        }
+      }
   }
 }
