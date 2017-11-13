fd20969d4ac385c097e46b364967459b3ef67505
Francisco Jesús Caballero Molina <fjcmolina7@gmail.com>
2017-11-13 09:48:39 +0100

    Actualizando ejercicio

--- Bucles/Ejercicio38.java
+++ Bucles/Ejercicio38.java
@@ -10,7 +10,7 @@
  public class Ejercicio38{
   public static void main(String[] args) {
     
-    System.out.print("Introduce la altura de la X(numero impar igual o mayor que 3): ");    
+    System.out.print("Introduce la altura del reloj de arena: ");    
     int alturaIntroducida = Integer.parseInt(System.console().readLine());
     
     int altura = 1;
