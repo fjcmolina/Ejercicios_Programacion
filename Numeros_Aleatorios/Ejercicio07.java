b1604977c2488a674acc966bb1c490c7c1beeb91
Francisco Jesús Caballero Molina <fjcmolina7@gmail.com>
2017-11-21 10:27:24 +0100

    Actualizando ejercicios

--- Numeros_Aleatorios/Ejercicio07.java
+++ Numeros_Aleatorios/Ejercicio07.java
@@ -7,5 +7,30 @@
  
  public class Ejercicio07{
   public static void main(String[] args) {
+    
+    int columnas = 3;
+    
+    for(int fila = 1; fila <= 15; fila++){
+      System.out.printf("%4d) ", fila);
+      
+      if(fila==15){
+        columnas = 1;
+        }
+      for(int apuesta = 1; apuesta <= columnas; apuesta++){
+        int resultado = (int)(Math.random()*3) + 1;
+        switch(resultado){
+          case 1:
+            System.out.print(" 1 |");
+            break;
+          case 2:
+            System.out.print(" X |");
+            break;
+          case 3:
+            System.out.print(" 2 |");
+            break;
+          }
+      }
+      System.out.println();
+    }
   }
 }
