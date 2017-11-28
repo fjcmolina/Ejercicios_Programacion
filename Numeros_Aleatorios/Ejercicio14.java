31cff83f8b3a2d893aaa913ff8ae891e9de87653
Francisco Jesús Caballero Molina <fjcmolina7@gmail.com>
2017-11-28 11:00:52 +0100

    Actualizando

---
+++ Numeros_Aleatorios/Ejercicio14.java
@@ -0,0 +1,50 @@
+/**
+ * Numeros Aleatorios
+ *
+ * 14. Realiza un programa que haga justo lo contrario a lo que hace el ejercicio 6.
+ *     El programa intentará adivinar el número que estás pensando - un número
+ *     entre 0 y 100 - teniendo para ello 5 oportunidades. En cada intento fallido, el
+ *     programa debe preguntar si el número que estás pensando es mayor o menor
+ *     que el que te acaba de decir.
+ */ 
+ 
+ public class Ejercicio14{
+  public static void main(String[] args) {
+    
+    System.out.println("Vamos a realizar un programa para que adivines un numero aleatorio");
+    
+    int oportunidades = 5;
+    int numeroPensado;
+    int minimo = 0;
+    int maximo = 100;
+    boolean acertado = false;
+    int mayorMenorOIgual;
+        
+    System.out.println("Piensa un número del 0 al 100. Intentaré adivinarlo en 5 intentos.");
+    System.out.println("Pulsa la tecla INTRO para empezar");
+    System.console().readLine();
+
+    do {
+      numeroPensado = (int)(Math.random() * (maximo - minimo) + minimo);
+      System.out.println("¿Es el " + numeroPensado + "?");
+      System.out.print("El número que estás pensando es 1(mayor) 2(menor) 3(Acertaste): ");
+      mayorMenorOIgual = Integer.parseInt(System.console().readLine());
+      oportunidades--;
+
+      if ( (mayorMenorOIgual == 1) && (oportunidades > 0) )
+        minimo = numeroPensado + 1;
+      
+      if ( (mayorMenorOIgual == 2) && (oportunidades > 0) )
+        maximo = numeroPensado - 1;
+      
+      if (mayorMenorOIgual == 3) {
+        acertado = true;
+        System.out.println("¡Bien! ¡He acertado!");
+      }
+    } while(!acertado && (oportunidades > 0));
+    
+    if (!acertado) {
+      System.out.println("Vaya, no he conseguido acertar el número.");
+    }
+  }
+}
