5bcf870bb58491e7da83e1633a97ba9d2ecb545f
Francisco Jesús Caballero Molina <32089045+fjcmolina@users.noreply.github.com>
2018-01-23 10:46:51 +0100

    Update Ejercicio05.java

--- Arrays Bidimensionales/Ejercicio05.java
+++ Arrays Bidimensionales/Ejercicio05.java
@@ -1,7 +1,7 @@
 /*
  *   Arrays Bidimensionales
  *  
- * 6. Realiza un programa que rellene un array de 6 filas por 10 columnas con
+ * 5. Realiza un programa que rellene un array de 6 filas por 10 columnas con
  *    números enteros positivos comprendidos entre 0 y 1000 (ambos incluidos).
  *    A continuación, el programa deberá dar la posición tanto del máximo como
  *    del mínimo.
