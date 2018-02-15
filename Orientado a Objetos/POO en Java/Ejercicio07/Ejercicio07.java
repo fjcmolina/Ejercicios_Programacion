e5403633b0bf74808d1cad749137cc53bfaf2148
fjcmolina <fjcmolina7@gmail.com>
2018-02-15 16:24:25 +0100

    Actualizando

--- Orientado a Objetos/POO en Java/Ejercicio07/Ejercicio07.java
+++ Orientado a Objetos/POO en Java/Ejercicio07/Ejercicio07.java
@@ -23,25 +23,49 @@ public class Ejercicio07 {
     Zona vip = new Zona(25);
 
     int opcion = 0;
+    int opcion2 = 0;
+    int n = 0;
 
-    while (opcion != 3) {
+    do {
       System.out.println("1. Mostar numero de entradas libres");
       System.out.println("2. Vender entradas");
       System.out.println("3. Salir");
       System.out.println();
       System.out.println("Elige una de las anteriores opciones");
       opcion = s.nextInt();
+      System.out.println();
+      
+      if (opcion == 1) {
 
-      switch (opcion) {
-        case 1:
-          System.out.println(principal.getEntradasPorVender());
-          break;
-        case 2:
-          System.out.println();
-          break;
-        default:
+        System.out.println("En la zona principal hay " + principal.getEntradasPorVender());
+        System.out.println("En la zona compraVenta hay " + compraVenta.getEntradasPorVender());
+        System.out.println("En la zona vip hay " + vip.getEntradasPorVender());
       }
-    }
-  }
+      System.out.println();
+      
+      if (opcion == 2) {
+        System.out.println("1.Principal");
+        System.out.println("2.CompraVenta");
+        System.out.println("3.Vip");
+        System.out.println("Elige una de las anteriores opciones");
+        opcion2 = s.nextInt();
 
+        System.out.println("¿Cuantas entradas quieres?");
+        n = s.nextInt();
+
+        switch (opcion2) {
+          case 1:
+            principal.vender(n);
+            break;
+          case 2:
+            compraVenta.vender(n);
+            break;
+          case 3:
+            vip.vender(n);
+            break;
+          default:
+        }
+      }
+    } while (opcion < 3);
+  }
 }
