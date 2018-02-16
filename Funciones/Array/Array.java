332f5170fe22f0d710dad457319526d3d86b83fa
fjcmolina <fjcmolina7@gmail.com>
2018-02-16 11:27:14 +0100

    Actualizando

--- Funciones/Array/Array.java
+++ Funciones/Array/Array.java
@@ -10,7 +10,7 @@ package array;
 public class Array {
 
   /**
-   * Genera un array de tamaño n con números aleatorios cuyo intervalo (mínimo y
+   * 1.Genera un array de tamaño n con números aleatorios cuyo intervalo (mínimo y
    * máximo) se indica como parámetro.
    *
    * @param n tamaño del array
@@ -26,7 +26,7 @@ public class Array {
   }
 
   /**
-   * Devuelve el mínimo del array que se pasa como parámetro.
+   * 2.Devuelve el mínimo del array que se pasa como parámetro.
    *
    * @param n array de enteros
    * @return minimo del array
@@ -44,7 +44,7 @@ public class Array {
   }
 
   /**
-   * Devuelve el maximo del array que se pasa como parámetro.
+   * 3.Devuelve el maximo del array que se pasa como parámetro.
    *
    * @param n array de enteros
    * @return maximo del array
@@ -62,7 +62,7 @@ public class Array {
   }
 
   /**
-   * Devuelve la media del array que se pasa como parámetro.
+   * 4.Devuelve la media del array que se pasa como parámetro.
    *
    * @param x array de numeros enteros
    * @return media del array
@@ -77,10 +77,39 @@ public class Array {
 
     return media;
   }
+
   /**
-   * Devuelve la media del array que se pasa como parámetro.
+   * 5.Dice si un número está o no dentro de un array.
    *
-   * @param x array de numeros enteros
-   * @return media del array
+   * @param n, número entero buscado
+   * @param x, array número entero donde buscar
+   * @return verdadero si el número está en el array, falso si no está en el
+   * array
    */
+  public static boolean estaEnArrayInt(int n, int[] x) {
+    if (posicionEnArray(n, x) >= 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  /**
+   * 6.Devuelve la posición de un número, n, dentro de un array de número enteros,
+   * si el número no está en el array devuelve -1.
+   *
+   * @param n número entero buscado
+   * @param x array de números enteros
+   * @return número entero volteado
+   */
+  public static int posicionEnArray(int n, int[] x) {
+
+    for (int i = 0; i < x.length; i++) {
+      if (x[i] == n) {
+        return i;
+      }
+    }
+    return -1;
+
+  }
 }
