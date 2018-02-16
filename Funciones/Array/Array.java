c73293441864cb311f8f016234b18a74f5de4915
fjcmolina <fjcmolina7@gmail.com>
2018-02-16 11:35:06 +0100

    Actualizando

--- Funciones/Array/Array.java
+++ Funciones/Array/Array.java
@@ -10,8 +10,8 @@ package array;
 public class Array {
 
   /**
-   * 1.Genera un array de tamaño n con números aleatorios cuyo intervalo (mínimo y
-   * máximo) se indica como parámetro.
+   * 1.Genera un array de tamaño n con números aleatorios cuyo intervalo (mínimo
+   * y máximo) se indica como parámetro.
    *
    * @param n tamaño del array
    * @return Array con n números aleatorios entre min y max
@@ -95,8 +95,8 @@ public class Array {
   }
 
   /**
-   * 6.Devuelve la posición de un número, n, dentro de un array de número enteros,
-   * si el número no está en el array devuelve -1.
+   * 6.Devuelve la posición de un número, n, dentro de un array de número
+   * enteros, si el número no está en el array devuelve -1.
    *
    * @param n número entero buscado
    * @param x array de números enteros
@@ -110,6 +110,58 @@ public class Array {
       }
     }
     return -1;
+  }
 
+  /**
+   * 7.Devuelve un array con los valores volteados del array pasado por
+   * parametro.
+   *
+   * @param x array de números enteros
+   * @return array de números enteros con los valores volteados
+   */
+  public static int[] volteaArrayInt(int[] x) {
+
+    int[] voltea = new int[x.length];
+    for (int i = 0; i < x.length; i++) {
+      voltea[i] = x[x.length - i - 1];
+    }
+    return voltea;
+  }
+
+  /**
+   * 8.Devuelve el array pasado como parametro tras rotarlo n posiciones a la
+   * izquierda
+   *
+   * @param n número entero, posiciones a rotar
+   * @param x Array de números enteros,
+   * @return  Array de números enteros, tras rotar el array n
+   * posiciones a la derecha
+   */
+
+  public static int[] rotaDerechaArrayInt(int n, int[] x) {
+
+    int[] voltea = new int[x.length];
+    n = x.length - n;
+    for (int i = 0; i < x.length; i++) {
+      if ((i + n) >= x.length) {
+        n = -i;
+      }
+      voltea[i] = x[i + n];
+    }
+    return voltea;
+
+  }
+
+  /**
+   * 9.Devuelve el array pasado como parametro tras rotarlo n posiciones a la
+   * izquierda
+   *
+   * @param n número entero, posiciones a rotar
+   * @param , Array de números enteros
+   * @return  Array de números enteros, tras rotar el array n
+   * posiciones a la izquierda
+   */
+  public static int[] rotaIzquierdaArrayInt(int n, int[] x) {
+    return rotaDerechaArrayInt((x.length - n), x);
   }
 }
