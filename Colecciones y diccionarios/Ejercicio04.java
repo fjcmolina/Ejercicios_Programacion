6b7f04cf2219065d6ff039ee70a3de0c66fdcf18
fjcmolina <fjcmolina7@gmail.com>
2018-04-05 09:00:20 +0200

    Actualizando

--- Colecciones y diccionarios/Ejercicio04.java
+++ Colecciones y diccionarios/Ejercicio04.java
@@ -8,6 +8,7 @@ package ejercicio04;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Scanner;
 /**
  *
  * @author Francis
@@ -15,28 +16,23 @@ import java.util.Collections;
 public class Ejercicio04 {
 
   public static void main(String[] args) {
-    
+     Scanner s = new Scanner(System.in);
       ArrayList<String> a = new ArrayList<>();
 
-    a.add("Hola");
-    a.add("Coche");
-    a.add("Moto");
-    a.add("Bicicleta");
-    a.add("Furgoneta");
-    a.add("Avion");
-    a.add("Tractor");
-    a.add("Camion");
-    a.add("Adios");
-    a.add("Tren");
+    System.out.println("Introduce 10 palabras:");
+    for (int i = 0; i < 10; i++) {
+      String palabra = s.next();
+      a.add(palabra);
+    }
 
-    System.out.println("\nNúmeros en el orden original:");
+    System.out.println("\nPalabras en el orden original:");
     for (String palabra : a) {
       System.out.println(palabra);
     }
 
     Collections.sort(a);
 
-    System.out.println("\nNúmeros en el orden original:");
+    System.out.println("\nPalabras ordenadas alfabeticamente:");
     for (String palabra : a) {
       System.out.println(palabra);
     }
