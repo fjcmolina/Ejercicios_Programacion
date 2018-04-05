54e93df016e5a8c90f1f07c0b035fbb97814fdd3
fjcmolina <fjcmolina7@gmail.com>
2018-04-05 08:45:55 +0200

    Actualizando

--- Colecciones y diccionarios/Ejercicio02.java
+++ Colecciones y diccionarios/Ejercicio02.java
@@ -17,17 +17,32 @@ import java.util.ArrayList;
 public class Ejercicio02 {
 
   public static void main(String[] args) {
-    
+
     ArrayList<Integer> a = new ArrayList<>();
-    
-    int variable = (int)(Math.random() * 11 ) + 10;
-    
-    for(int i = 0; i < variable;i++){
-      int num = (int)(Math.random() * 100);
+
+    int variable = (int) (Math.random() * 11) + 10;
+    int suma = 0;
+    int max = 0;
+    int min = 999;
+
+    for (int i = 0; i < variable; i++) {
+      int num = (int) (Math.random() * 101);
       a.add(num);
+      suma += num;
+
+      if (num < min) {
+        min = num;
+      }
+      if (num > max) {
+        max = num;
+      }
     }
-    
+
     System.out.println(a);
+    System.out.println("Suma " + suma);
+    System.out.println("Minimo " + min);
+    System.out.println("Maximo " + max);
+    System.out.println("Media " + suma / variable);
   }
-  
+
 }
